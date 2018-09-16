40964ad02878091e6399dfec0f14fe9130e44557
Anton Dmitrievsky <admitrievsky@gmail.com>
2018-09-16 23:57:53 +0500

    Add CatScoreService tests

--- src/app/services/cat-score.service.spec.ts
+++ src/app/services/cat-score.service.spec.ts
@@ -1,12 +1,61 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CatScoreService } from './cat-score.service';
+import { LocalStorageService } from './local-storage.service';
+import { project } from '../../testing/model-stubs/project-stub';
+
+class LocalStorageMockService {
+  private values: {key: string, value: string}[] = [];
+
+  get(key: string): string {
+    return this.values[key] || null;
+  }
+
+  set(key: string, value: string) {
+    this.values[key] = value;
+  }
+}
 
 describe('CatScoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CatScoreService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        CatScoreService,
+        {provide: LocalStorageService, useClass: LocalStorageMockService}
+      ]
+    });
+    service = TestBed.get(CatScoreService);
+  });
 
   it('should be created', () => {
-    const service: CatScoreService = TestBed.get(CatScoreService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return zero for unknown project', () => {
+    expect(service.get(project)).toBe(0);
+  });
+
+  it('should return 1 for on increment', () => {
+    expect(service.increase(project)).toBe(1);
+    expect(service.get(project)).toBe(1);
+  });
+
+  it('should return 2 for twice increment', () => {
+    expect(service.increase(project)).toBe(1);
+    expect(service.increase(project)).toBe(2);
+    expect(service.get(project)).toBe(2);
+  });
+
+  it('should return 0 for decrement after increment', () => {
+    expect(service.increase(project)).toBe(1);
+    expect(service.decrease(project)).toBe(0);
+    expect(service.get(project)).toBe(0);
+  });
+
+  it(`shouldn't go to negative side`, () => {
+    expect(service.decrease(project)).toBe(0);
+    expect(service.get(project)).toBe(0);
   });
 });
---
+++ src/testing/model-stubs/project-stub.ts
@@ -0,0 +1,12 @@
+import { Project } from "../../app/models/github-responses";
+
+export const project: Project = {
+  id: 123,
+  forks_count: 0,
+  stargazers_count: 0,
+  score: 0,
+  watchers_count: 0,
+  full_name: 'full name',
+  description: 'description',
+  owner: {avatar_url: 'url', login: 'login'}
+};
