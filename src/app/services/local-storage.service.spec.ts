fa3db6769d4582f8628a683407085734857a65cb
Anton Dmitrievsky <admitrievsky@gmail.com>
2018-09-17 00:16:55 +0500

    Add LocalStorageService tests

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -3,10 +3,34 @@ import { TestBed } from '@angular/core/testing';
 import { LocalStorageService } from './local-storage.service';
 
 describe('LocalStorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: LocalStorageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(LocalStorageService);
+  });
 
   it('should be created', () => {
-    const service: LocalStorageService = TestBed.get(LocalStorageService);
     expect(service).toBeTruthy();
+  });
+
+  it('should call localStorage.getItem', () => {
+    const returnValue = 'return value';
+
+    const spy = spyOn(localStorage.__proto__, 'getItem');
+    spy.and.returnValue(returnValue);
+
+    expect(service.get('test')).toBe(returnValue);
+    expect(spy.calls.count()).toBe(1);
+    expect(spy.calls.mostRecent().args[0]).toBe('test');
+  });
+
+  it('should call localStorage.putItem', () => {
+    const spy = spyOn(localStorage.__proto__, 'setItem');
+
+    service.set('test', 'value');
+    expect(spy.calls.count()).toBe(1);
+    expect(spy.calls.mostRecent().args[0]).toBe('test');
+    expect(spy.calls.mostRecent().args[1]).toBe('value');
   });
 });
