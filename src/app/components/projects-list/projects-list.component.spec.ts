b9e72f6a586128368e50eb5212f60c0662dd5fdd
Anton Dmitrievsky <admitrievsky@gmail.com>
2018-09-22 15:40:49 +0500

    Add tests for Project List component

--- src/app/components/projects-list/projects-list.component.spec.ts
+++ src/app/components/projects-list/projects-list.component.spec.ts
@@ -1,14 +1,36 @@
+import { Component, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { ProjectsListComponent } from './projects-list.component';
+import { GithubConnectorService } from '../../services/github-connector.service';
+import { searchResults } from '../../../testing/model-stubs/project-stub';
+
+@Component({selector: 'app-cat-score', template: ''})
+class CatScoreComponentStub {
+  @Input() project;
+}
 
 describe('ProjectsListComponent', () => {
   let component: ProjectsListComponent;
   let fixture: ComponentFixture<ProjectsListComponent>;
+  let githubConnectorService: jasmine.SpyObj<GithubConnectorService>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
+    githubConnectorService = jasmine.createSpyObj(
+      'GithubConnectorService', ['searchRepositories']);
+    githubConnectorService.searchRepositories.and.returnValue(
+      of(searchResults));
+
     TestBed.configureTestingModule({
-      declarations: [ ProjectsListComponent ]
+      declarations: [
+        ProjectsListComponent,
+        CatScoreComponentStub
+      ],
+      providers: [
+        { provide: GithubConnectorService, useValue: githubConnectorService }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +38,37 @@ describe('ProjectsListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectsListComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search projects at start', () => {
+    expect(githubConnectorService.searchRepositories.calls.count()).toBe(1);
+  });
+
+  it('should search Angular only projects', () => {
+    expect(githubConnectorService.searchRepositories.calls
+      .mostRecent().args[0]).toBe('angular');
+  });
+
+  it('should contain project with name `full name`', () => {
+    const div:HTMLDivElement =
+      element.querySelector('.project:nth-child(1) .project__title');
+    expect(div.textContent).toBe('full name');
+  });
+
+  it('should contain project with name `Another full name`', () => {
+    const div:HTMLDivElement =
+      element.querySelector('.project:nth-child(2) .project__title');
+    expect(div.textContent).toBe('Another full name');
+  });
+
+  it('creates cat score component for records', () => {
+    expect(
+      element.querySelector('.project:nth-child(1) app-cat-score')).toBeTruthy();
   });
 });
--- src/testing/model-stubs/project-stub.ts
+++ src/testing/model-stubs/project-stub.ts
@@ -1,5 +1,7 @@
 import { Project, SearchResults } from "../../app/models/github-responses";
 
+const catImage = 'data:image/gif;base64,R0lGODlhCgAMAPUAAAAAAN52LNuHKNaOLN6JKd2MKcuXMsqWM+iKLeqMLeeSKuKXLumTKeiTKumUKuqVKuqVK+uWK+iVLOqVLOuVLeiVL+2YK++ZKu6ZK++ZK+iZLOuYL/GbK/OcKfSdKfCcLPGcLPOdLPSeLOmWMPOfMPSeMe6lS+mjTvCjQvGmRuOnZfW3ZPi7auu4fvS9d+Gzhe/BiuvAkePFrfHRrujQvfDUtvrbtO/czO7czffjzffw6gAAAAAAAAAAAAAAAAAAACH5BAEAAAAALAAAAAAKAAwAAAZVQIBwKKwJA5QPQrgZDg4LAwHQGBYkCY1AqBiiSqGH0DHMsTwhi7AK0NlIIlEGQAbEcqkLJh0RVlomDCBxEEIjABMNHR4cQzMvMCouKydDNDI4ADdDQQA7';
+
 export const project: Project = {
   id: 123,
   forks_count: 0,
@@ -8,7 +10,7 @@ export const project: Project = {
   watchers_count: 0,
   full_name: 'full name',
   description: 'description',
-  owner: {avatar_url: 'url', login: 'login'}
+  owner: {avatar_url: catImage, login: 'login'}
 };
 
 export const anotherProject: Project = {
@@ -17,9 +19,9 @@ export const anotherProject: Project = {
   stargazers_count: 0,
   score: 0,
   watchers_count: 0,
-  full_name: 'full name',
-  description: 'description',
-  owner: {avatar_url: 'url', login: 'login'}
+  full_name: 'Another full name',
+  description: 'Another description',
+  owner: {avatar_url: catImage, login: 'login'}
 };
 
 export const searchResults: SearchResults = {
