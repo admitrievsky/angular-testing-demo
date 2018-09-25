f128e49c6737abb9eaa85e42aeca913d59dca8cf
Anton Dmitrievsky <admitrievsky@gmail.com>
2018-09-26 01:23:29 +0500

    Add e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,23 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to testing-demo!');
+    expect(page.getParagraphText()).toEqual('Testing demo: 🐱CatHub🐱');
+  });
+
+  it('should fetch projects from github', () => {
+    page.navigateTo();
+    expect(page.getProjects().count()).toEqual(30);
+  });
+
+  it('should fetch first project as angular', () => {
+    page.navigateTo();
+    expect(page.getFirstProjectTitle()).toEqual('angular/angular');
+  });
+
+  it('should increase score by one', () => {
+    page.navigateTo();
+    page.getFirstProjectPlusButton().click().then(() => {
+      expect(page.getFirstProjectScore()).toEqual("1");
+    });
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,21 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getProjects() {
+    const projects = element.all(by.css('.project'));
+    return projects;
+  }
+
+  getFirstProjectTitle() {
+    return element(by.css('.project:first-child .project__title')).getText();
+  }
+
+  getFirstProjectPlusButton() {
+    return element(by.css('.project:first-child .cat-score__btn.increase'));
+  }
+
+  getFirstProjectScore() {
+    return element(by.css('.project:first-child .cat-score__value')).getText();
+  }
 }
